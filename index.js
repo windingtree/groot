eea7d97adea69c1c5654d7c6b395dcec9098dfff
Leandro <leandro.boscariol@gnosis.io>
2022-02-11 09:54:24 -0800

    Added auto-replies for many airdrop related questions

--- index.js
+++ index.js
@@ -20,6 +20,16 @@ const secretChannel = /^!join$/
 const noCommands = /^!/
 const noChannelTags = /^\s*\<#\d+\>\s*$/
 
+// auto-replies
+const wenToken = /.*wh?en (token|airdrop|drop|claim).*/i
+const whereToken = /.*where (to |.*)(claim|airdrop).*/i
+const howToClaim = /.*(how) (.*)(claim|airdrop).*/i
+const whenTrade = /.*(wh?en|how|where) .*(trade|exchange|swap|sell).*/i
+const whenGovernance = /a/i
+const sellToken = /.*(where|how|wh?en).*(sell).*/i
+const tokenPrice = /.*(what)?.*(token)? price.*/i
+const wenMoon = /.*wh?en.*(mo+n|lambo).*/i
+
 function codeBlock(message) {
   return '```' + message + '```'
 }
@@ -61,6 +71,18 @@ client.on('messageCreate', async (message) => {
         'Please mooooove your `gm` and `gn` to the #gm channel'
       )
       await message.delete()
+    } else if (wenToken.test(message.content)) {
+      await message.reply('Soon! Check #announcements channel')
+    } else if (whereToken.test(message.content)) {
+      await message.reply('It will be visible on CowSwap once it is ready')
+    } else if (howToClaim.test(message.content)) {
+      await message.reply('Follow the instructions on https://medium.com/@cow-protocol/step-by-step-guide-for-claiming-vcow-in-gnosis-chain-b1a1442a3454')
+    } else if (whenTrade.test(message.content) || sellToken.test(message.content)) {
+      await message.reply('vCOW is a non-transferable governance token. CowDAO might vote to make it swapable via governance vote.\nWen? After airdop is complete\nWen? 6 weeks after initial deployment\nWen? Aprox. March 25th')
+    } else if (tokenPrice.test(message.content)) {
+      await message.reply('The price for investing is 0.15 USD per vCOW. The equivalent in GNO, ETH and xDAI (according to what option you have, if any) was defined at the proposal creation time.\nSee https://forum.gnosis.io/t/gip-13-phase-2-cowdao-and-cow-token/2735 or Pinned messages on #general for more details')
+    } else if (wenMoon.test(message.content)) {
+      await message.reply('<imagine a funny moon related joke gif>')
     }
   } catch (e) {
     console.error('Something failed handling a message', e)
